1654463b821a19357318f702ab839e4d3c5c4631
binayak1320 <csebinayak.ku@gmail.com>
2025-12-06 14:33:46 +0600

    feat: add responsive menu toggle functionality with hamburger and close icons

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,6 +5,9 @@ import coinbase_logo from "./coinbase_logo.png";
 import airbnb_logo from "./airbnb_logo.svg";
 import google_logo from "./google_logo.svg";
 import arrow_icon from "./arrow_icon.svg";
+import close_icon from "./close_icon.svg";
+import menu_icon_dark from "./menu_icon_dark.svg";
+import menu_icon from "./menu_icon.svg";
 
 export const company_logos = [
   microsoft_logo,
@@ -17,6 +20,9 @@ export const company_logos = [
 
 const assets = {
   arrow_icon,
+  close_icon,
+  menu_icon_dark,
+  menu_icon,
 };
 
 export default assets;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import assets from "../assets/assets";
 const DarkLogo = () => {
   return (
@@ -28,32 +28,50 @@ const LightLogo = () => {
   );
 };
 const Navigation = ({ theme, setTheme }) => {
+  const [openSidebar, setOpenSidebar] = useState(false);
   return (
     <div className="flex px-4 sm:px-12 lg:px-24 xl:px-40 py-4 backdrop-blur-xl justify-between items-center sticky top-0 z-20 font-medium bg-white/50 dark:bg-gray-900/70">
       {theme === "dark" ? <DarkLogo /> : <LightLogo />}
-      <div className="text-gray-700 dark:text-white top-0 bottom-0 right-0 flex gap-5 transition-all sm:text-sm max-sm:w-60 max-sm:pl-10 max-sm:fixed max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 sm:items-center ">
-        
-        
-        <a href="" className="sm:hover:bottom-b">
+      <div
+        className={`text-gray-700 dark:text-white top-0 bottom-0 right-0 flex gap-5 transition-all sm:text-sm ${
+          !openSidebar
+            ? "max-sm:w-0 overflow-hidden"
+            : "max-sm:w-60 max-sm:pl-10"
+        } max-sm:fixed max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 sm:items-center`}
+      >
+        <img
+          onClick={() => setOpenSidebar(false)}
+          src={assets.close_icon}
+          alt=""
+          className="w-5 right-4 top-4 sm:hidden absolute"
+        />
+
+        <a onClick={() => setOpenSidebar(false)} href="" className="sm:hover:border-b">
           Home
         </a>
-        <a href="#services" className="sm:hover:bottom-b">
+        <a onClick={() => setOpenSidebar(false)} href="#services" className="sm:hover:border-b">
           Services
         </a>
-        <a href="" className="sm:hover:bottom-b">
+        <a onClick={() => setOpenSidebar(false)} href="" className="sm:hover:border-b">
           About
         </a>
-        <a href="#contact-us" className="sm:hover:bottom-b">
+        <a onClick={() => setOpenSidebar(false)} href="#contact-us" className="sm:hover:border-b">
           Contact us
         </a>
-        <a href="#our-work" className="sm:hover:bottom-b">
+        <a onClick={() => setOpenSidebar(false)} href="#our-work" className="sm:hover:border-b">
           Our work
         </a>
       </div>
-      <div>
+      <div className="flex items-center gap-2 sm:gap-4">
+        <img
+          onClick={() => setOpenSidebar(true)}
+          src={theme === "dark" ? assets.menu_icon:assets.menu_icon_dark }
+          alt=""
+          className="w-8 sm:hidden"
+        />
         <a
           href="#contact-us"
-          className="text-sm flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full hover:scale-103 transition-all"
+          className="text-sm flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full hover:scale-103 transition-all max-sm:hidden cursor-pointer"
         >
           Connect <img src={assets.arrow_icon} width={14} alt="" />
         </a>
