428724cab67fd7e6f083bd720ac01798c9849edc
binayak1320 <csebinayak.ku@gmail.com>
2025-12-07 10:14:15 +0600

    fix: remove some un-necessary code

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,6 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navigation from "./components/Navigation";
-import Hero from "./components/Hero";
-import Partnership from "./components/Partnership";
-import Services from "./components/Services";
-import Testimonial from "./components/Testimonial";
-import Teams from "./components/Teams";
-import ContactUs from "./components/ContactUs";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -30,13 +24,6 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
         </Routes>
          <Footer theme={theme}/>
-        {/* <Hero />
-      <Partnership />
-      <Services />
-      <Testimonial />
-      <Teams />
-      <ContactUs />
-      */}
       </div>
 
     </Router>
