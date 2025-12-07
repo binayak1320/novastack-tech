d5178bd4256607df224072268cc3baed3b6b1de3
binayak1320 <csebinayak.ku@gmail.com>
2025-12-07 10:47:37 +0600

    Fix: CTA button hidden issue in mobile screen

--- src/components/CTAButton.jsx
+++ src/components/CTAButton.jsx
@@ -5,7 +5,7 @@ const CTAButton = ({ href, label }) => {
   return (
     <a
       href={href}
-      className="text-sm flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full hover:scale-103 transition-all max-sm:hidden cursor-pointer"
+      className="text-sm flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full hover:scale-103 transition-all cursor-pointer"
     >
       {label} <img src={assets.arrow_icon} width={14} alt="" />
     </a>
